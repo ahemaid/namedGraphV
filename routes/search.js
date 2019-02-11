ea2065addb350c4b10a5d5ec03301bfce3d6b813
ahmad <ahmedcertsoft@gmail.com>
2019-02-11 13:46:45 +0100

    add

--- routes/search.js
+++ routes/search.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var fs = require('fs');
 
 router.get('/', function(req, res) {
-  var searchString = req.originalUrl.split("search?")[1]; 
+  var searchString = ""; 
+  if(req.originalUrl.split("search?")[1])
+  searchString = req.originalUrl.split("search?")[1] ; 
     res.render('search', {
       title: 'search',
       searchTerm : searchString
